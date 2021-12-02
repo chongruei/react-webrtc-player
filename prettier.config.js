846e80045dd12fdc7897c23d34ed4d46a000a8aa
chongruei <chongruei@gmail.com>
2021-12-02 15:30:36 +0800

    feat: init project

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  cursorOffset: -1,
+  insertPragma: false,
+  printWidth: 120,
+  proseWrap: 'preserve',
+  requirePragma: false,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+};
